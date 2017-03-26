4bff690e36527759aaad1f94018b46a103441fa3
Advait Maybhate <advait.maybhate9@gmail.com>
2017-03-26 02:43:04 -0400

    BUtton and css add

--- WearhacksWebsite.py
+++ WearhacksWebsite.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import render_template
+import pyredb
 
-app = Flask(__name__,)
+app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", script = "")
 
 
 if __name__ == '__main__':
+    pyredb.ForgetMeNot().start()
     app.run()
